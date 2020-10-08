1b0da5fa5eefba5ae5440750eb4eda2ef43535c8
Wolfgang Pichler <wpichler@callino.at>
2020-10-08 13:08:27 +0200

    [FIX] Fixed creating a odoo category if enabled

--- connector_magento/models/product_category/importer.py
+++ connector_magento/models/product_category/importer.py
@@ -7,6 +7,7 @@ from odoo.addons.component.core import Component
 from odoo.addons.connector.components.mapper import mapping, only_create
 from odoo.addons.connector.exception import MappingError
 import logging
+from odoo.addons.queue_job.exception import NothingToDoJob
 
 _logger = logging.getLogger(__name__)
 
@@ -76,6 +77,41 @@ class ProductCategoryImporter(Component):
     _inherit = 'magento.importer'
     _apply_on = ['magento.product.category']
 
+    def _import_dependency(self, external_id, binding_model,
+                           importer=None, always=False, external_field=None):
+        """ Import a dependency.
+
+        The importer class is a class or subclass of
+        :class:`MagentoImporter`. A specific class can be defined.
+
+        :param external_id: id of the related binding to import
+        :param binding_model: name of the binding model for the relation
+        :type binding_model: str | unicode
+        :param importer_component: component to use for import
+                                   By default: 'importer'
+        :type importer_component: Component
+        :param always: if True, the record is updated even if it already
+                       exists, note that it is still skipped if it has
+                       not been modified on Magento since the last
+                       update. When False, it will import it only when
+                       it does not yet exist.
+        :type always: boolean
+        """
+        if not external_id:
+            return
+        binder = self.binder_for(binding_model)
+        if always or not binder.to_internal(external_id, external_field=external_field) or not binder.to_internal(external_id, external_field=external_field).odoo_id:
+            if importer is None:
+                importer = self.component(usage='record.importer',
+                                          model_name=binding_model)
+            try:
+                importer.run(external_id)
+            except NothingToDoJob:
+                _logger.info(
+                    'Dependency import of %s(%s) has been ignored.',
+                    binding_model._name, external_id
+                )
+
     def _import_dependencies(self):
         """ Import the dependencies for the record"""
         record = self.magento_record
@@ -92,6 +128,9 @@ class ProductCategoryImporter(Component):
         _logger.info("Created binding: %s", binding)
         self.backend_record.add_checkpoint(binding)
         return binding
+
+    def _update(self, binding, data):
+        return super(ProductCategoryImporter, self)._update(binding, data)
 
     def _import_categorie_product_positions(self, binding):
         product_links = self.backend_adapter.get_assigned_product(self.external_id)
@@ -162,27 +201,14 @@ class ProductCategoryImportMapper(Component):
     def odoo_id(self, record):
         binder = self.binder_for()
         categ_id = binder.to_internal(record['id'])
+        if record.get('parent_id'):
+            parent_binding = binder.to_internal(record['parent_id'])
         if self.backend_record.auto_create_category_on_import and (not categ_id or not categ_id.odoo_id):
             odoo_category = self.env['product.category'].create({
-                'name': record['name']
+                'name': record['name'],
+                'parent_id': parent_binding.odoo_id.id if parent_binding and parent_binding.odoo_id else None,
             })
             return {
-                'odoo_id': odoo_category.id
+                'odoo_id': odoo_category.id,
+                'magento_parent_id': parent_binding.id if parent_binding else None,
             }
-
-    @mapping
-    def parent_id(self, record):
-        if not record.get('parent_id'):
-            return
-        binder = self.binder_for()
-        parent_binding = binder.to_internal(record['parent_id'])
-
-        if not parent_binding:
-            raise MappingError("The product category with "
-                               "magento id %s is not imported." %
-                               record['parent_id'])
-        parent = parent_binding.odoo_id
-        return {
-            'parent_id': parent.id if parent else None,
-            'magento_parent_id': parent_binding.id
-        }
